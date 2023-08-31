ae11aa2bc1c1389d9674fc8d60a4c91bfc9d2de6
Prateek Kishore Karnad <trainer.it.ssk@gmail.com>
2023-08-31 23:26:37 +0530

    dev prod seperation

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM adoptopenjdk/maven-openjdk11
+
+COPY target/eurekaserver-prod-1.jar app.jar
+
+EXPOSE 8070:8070
+
+RUN apt-get update
+RUN apt-get install -y gcc
+RUN apt-get install -y curl
+
+ENTRYPOINT ["java","-jar","app.jar"]
--- src/main/java/com/example/eurekaserver/EurekaserverApplication.java
+++ src/main/java/com/example/eurekaserver/EurekaserverApplication.java
@@ -2,6 +2,7 @@ package com.example.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
@@ -10,6 +11,10 @@ public class EurekaserverApplication
 {
     public static void main(String[] args)
     {
-        SpringApplication.run(EurekaserverApplication.class, args);
+
+        new SpringApplicationBuilder()
+                .profiles("dev")
+                .sources(EurekaserverApplication.class)
+                .run(args);
     }
 }
